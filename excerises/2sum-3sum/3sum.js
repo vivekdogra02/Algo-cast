035d27230b35cdf3e9f28dea77842d6d8d97975b
Vivek Dogra <vivek.dogra@nagarro.com>
2019-10-16 16:32:29 +0530

    Adding more solution to 3 sum algo

--- excerises/2sum-3sum/3sum.js
+++ excerises/2sum-3sum/3sum.js
@@ -180,3 +180,30 @@ var threeSum = function(nums) {
 	}
 	return rtn;
 };
+
+// OR
+
+var threeSum = function(nums) {
+   nums.sort((a, b) => a - b);
+   const r = [];
+   for (let i = 0; i < nums.length - 2; i++) {
+     if (nums[i] > 0) break;
+     if (i > 0 && nums[i] === nums[i - 1]) continue;
+     let left = i + 1;
+     let right = nums.length - 1;
+     while (left < right) {
+       const s = nums[i] + nums[left] + nums[right];
+       if (s < 0) left += 1;
+       else if (s > 0) right -= 1;
+       else {
+         r.push([nums[i], nums[left], nums[right]]);
+         while (left < right && nums[left] === nums[left + 1]) left += 1;
+         while (left < right && nums[right] === nums[right - 1]) right -= 1;
+         left += 1;
+         right -= 1;
+       }
+     }
+   }
+   
+   return r;
+ };
